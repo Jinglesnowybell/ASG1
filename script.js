2fbcb64181e58415d6c2c3a550dbbc378554ca51
Jinglesnowybell <115936422+Jinglesnowybell@users.noreply.github.com>
2022-12-03 04:49:06 +0000

    Changed Slide Codes

--- script.js
+++ script.js
@@ -8,33 +8,46 @@ function myFunction() {
     }
 }
   
-let slideIndex = 1;
-  showSlides(slideIndex);
-  
-// Next/previous controls
+const slideShows = document.querySelectorAll('.container');
+
+let slideShow, slideIndex; //will be set to the current slideShow as appropriate
+
+for (let i=0; i < slideShows.length; i++) {
+  slideShow = slideShows[i];
+  slideShow.setAttribute('data-slideIndex', 1); //instead of slideIndex = 1 we remember the index for each slideshow in a data attribute
+  slideIndex = 1;
+  showSlides(1);
+}
+
 function plusSlides(n) {
+  slideShow = event.target.parentElement;
+  slideIndex = Number(slideShow.getAttribute('data-slideIndex'));
   showSlides(slideIndex += n);
 }
-  
-// Thumbnail image controls
+
 function currentSlide(n) {
+  slideShow = event.target.parentElement.parentElement.parentElement;
+  slideIndex = Number(slideShow.getAttribute('data-slideIndex'));
   showSlides(slideIndex = n);
 }
-  
+
 function showSlides(n) {
-  let i;
-    let slides = document.getElementsByClassName("mySlides");
-     let dots = document.getElementsByClassName("demo");
-       let captionText = document.getElementById("caption");
-       if (n > slides.length) {slideIndex = 1}
-         if (n < 1) {slideIndex = slides.length}
-      for (i = 0; i < slides.length; i++) {
+  var i;
+  var slides = slideShow.getElementsByClassName("mySlides");
+  var dots = slideShow.getElementsByClassName("demo");
+  var captionText = slideShow.querySelector(".caption");
+  
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
+  }
+  for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    captionText.innerHTML = dots[slideIndex-1].alt;
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+  
+  slideShow.setAttribute('data-slideIndex', slideIndex); // remember the slideIndex for this slideShow
 }
